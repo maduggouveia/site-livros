7c6307286510ef599b2a89b95868a76773ed935e
Maria Eduarda Gouveia <maduggouveia@gmail.com>
2025-07-23 10:45:41 -0300

    testando pastas de rota para páginas secundárias

---
+++ src/app/editoriais/page.tsx
@@ -0,0 +1,13 @@
+export default function Editoriais() {
+  return (
+
+    <>
+
+    <h1>Serviços Editoriais</h1>
+
+    <p>testando para ver se aparece</p>
+
+    </>
+
+  );
+}
---
+++ src/app/meus-livros/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function BooksPage() {
+  return (
+
+    <>
+
+    <h1>Meus livros</h1>
+
+    <p>testando para ver se aparece</p>
+
+    </>
+
+  );
+}
